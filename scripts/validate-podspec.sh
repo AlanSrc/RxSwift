d7ed6ab76e2a90d26ba31ba349b83146371b31d8
Krunoslav Zaher <krunoslav.zaher@gmail.com>
2019-01-30 00:12:37 +0100

    Fix CI.

--- scripts/validate-podspec.sh
+++ scripts/validate-podspec.sh
@@ -16,8 +16,21 @@ function cleanup {
 trap cleanup EXIT
 
 VERSION=`cat RxSwift.podspec | grep -E "s.version\s+=" | cut -d '"' -f 2`
-TARGETS=(RxTest RxCocoa RxBlocking RxAtomic RxSwift)
 ROOTS=(2/e/c 3/c/1 8/5/5 f/7/9 a/b/1)
+
+SWIFT_VERSION=''
+
+if [ ! -z "$TARGET" ]
+then
+    TARGETS=("$TARGET")
+else
+    TARGETS=(RxTest RxCocoa RxBlocking RxAtomic RxSwift)
+fi
+
+if [ ! -z "$SWIFT_VERSION" ]
+then
+    SWIFT_VERSION="--swift-version=${SWIFT_VERSION}"
+fi
 
 pushd ~/.cocoapods/repos/master/Specs
 for TARGET in ${TARGETS[@]}
@@ -28,26 +41,21 @@ popd
 
 for TARGET in ${TARGETS[@]}
 do
-
-
     for ROOT in ${ROOTS[@]} ; do
         mkdir -p ~/.cocoapods/repos/master/Specs/${ROOT}/${TARGET}/${VERSION}
         rm       ~/.cocoapods/repos/master/Specs/${ROOT}/${TARGET}/${VERSION}/* || echo
         cat $TARGET.podspec |
         sed -E "s/s.source [^\}]+\}/s.source           = { :git => '${ESCAPED_SOURCE}', :branch => \'${BRANCH}\' }/" > ~/.cocoapods/repos/master/Specs/${ROOT}/${TARGET}/${VERSION}/${TARGET}.podspec
     done
-
 done
 
 function validate() {
     local PODSPEC=$1
 
-    pod lib lint $PODSPEC --verbose --no-clean --allow-warnings
+    pod lib lint $PODSPEC --verbose --no-clean --allow-warnings "${SWIFT_VERSION}"
 }
 
 for TARGET in ${TARGETS[@]}
 do
-
-validate ${TARGET}.podspec
-
+    validate ${TARGET}.podspec
 done
